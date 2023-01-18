705877e26ec008e28160d0ec1360eb2cd8b0254a
urgt <urinbaevg@gmail.com>
2023-01-18 17:43:32 +0500

    client

--- client/script.js
+++ client/script.js
@@ -89,7 +89,7 @@ const handleSubmit = async (e) => {
         // messageDiv.innerHTML = "..."
         loader(messageDiv)
 
-        const response = await fetch('http://localhost:5000/', {
+        const response = await fetch('https://gpt-chat-uzb.onrender.com/', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
